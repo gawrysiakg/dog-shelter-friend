e8c83753f0ff8d348ad8bb61a70f0b6d5f4eb328
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-03-06 23:51:29 +0100

    refactoring

--- src/main/java/com/example/dogshelter/DogShelterApplication.java
+++ src/main/java/com/example/dogshelter/DogShelterApplication.java
@@ -1,13 +1,9 @@
 package com.example.dogshelter;
 
-import com.example.dogshelter.service.VolunteerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class DogShelterApplication {
@@ -15,23 +11,6 @@ public class DogShelterApplication {
     public static void main(String[] args) {
         SpringApplication.run(DogShelterApplication.class, args);
     }
-
-
-
-//    @Bean
-//    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-//                                                                               SqlInitializationProperties properties, VolunteerService volunteerService) {
-//        // This bean ensures the database is only initialized when empty
-//        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
-//            @Override
-//            public boolean initializeDatabase() {
-//                if (volunteerService.getAllVolunteers().size() == 0L) {
-//                    return super.initializeDatabase();
-//                }
-//                return false;
-//            }
-//        };
-//    }
 
 
 }
