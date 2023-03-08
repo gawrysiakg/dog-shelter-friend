64bc0ada8ba2a23b8cff459befadbb8155fd9ca2
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-03-08 16:58:14 +0100

    swagger added

--- src/main/java/com/example/dogshelter/DogShelterApplication.java
+++ src/main/java/com/example/dogshelter/DogShelterApplication.java
@@ -1,10 +1,11 @@
 package com.example.dogshelter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+@OpenAPIDefinition
 @SpringBootApplication
 public class DogShelterApplication {
 
